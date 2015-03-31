0cd303175fde0e62a12109cc8310e1c2d898d7a7
tomdos <chimpy@seznam.cz>
2015-03-31 10:32:44 +0200

    basic tree implementation - create, add, compare

---
+++ tree.py
@@ -0,0 +1,20 @@
+
+
+class TreeNode:
+    def __init__(self, value = None, left = None, right = None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+    def setLeft(self, left):
+        self.left = left
+        
+    def setRight(self, right):
+        self.right = right
+        
+    def setValue(self, value):
+        self.value = value
+
+
+if __name__ == "__main__":
+    pass

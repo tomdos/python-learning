0d17415ef851388447e93685a1bfe6dead9b45ad
tomdos <chimpy@seznam.cz>
2015-04-09 08:14:57 +0200

    add strig fixed next node, still buggy :-(

--- prefix-tree.py
+++ prefix-tree.py
@@ -4,74 +4,82 @@ import sys
 class PrefixTree:
     def __init__(self):
         self._root = self.Node()
-        
+
     def __str__(self):
         return self._root.__str__()
-    
+
     def add(self, word):
         currentNode = self._root
-        
+
         for i in range(len(word)):
             nextNode = currentNode.getSiblingByKey(word[i])
             if nextNode == None:
                 break;
-                
+
+            currentNode = nextNode
+
         for j in range(i, len(word)):
             node = self.Node(word[j])
             currentNode.addSibling(node)
             currentNode = node
 
-    def _dbgPrint(self, node, str):        
+    def _dbgPrint(self, node, str):
+        if node == None:
+            return
+
         if node.getValue():
             str = str + node.getValue()
-            
+
         sys.stdout.write(str + ": ") # root
-            
+
         for key in node.getAllSiblings():
             sys.stdout.write(key + " ")
-            
+
         sys.stdout.write("\n")
-            
+
         for key in node.getAllSiblings():
             node = node.getSiblingByKey(key)
             self._dbgPrint(node, str)
-    
+
     def dbgPrint(self):
         self._dbgPrint(self._root, "")
-    
-    
+
+
     class Node:
         def __init__(self, value = None):
             self._value = value
             self._siblings = {}
-            
+
         def __str__(self):
             return "v: {} s: {}".format(self._value, self._siblings)
-        
+
         def getValue(self):
             return self._value
-            
+
         def setValue(self, value):
             self._value = value
-        
+
         def getAllSiblings(self):
             return self._siblings
-            
+
         def getSiblingByKey(self, key):
             try:
                 return self._siblings[key]
             except KeyError:
                 return None
-            
+
         def addSibling(self, node):
             self._siblings[node.getValue()] = node
-        
+
 
 def main():
-    words = (    
+    words = (
     'amok',
     'amoks',
-    'amole',
+    'amole'
+    )
+
+    tst=(
     'amoles',
     'among',
     'amongst',
@@ -91,17 +99,15 @@ def main():
     'amorphously',
     'amorphousness',
     )
-    
+
     tree = PrefixTree()
     for w in words:
         tree.add(w)
         #print(w)
-    
+
     #print (tree)
     tree.dbgPrint()
-    
-        
+
+
 if __name__ == "__main__":
     main()
- 
-    
